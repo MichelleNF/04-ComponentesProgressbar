aa1dc4816f4fd800ae2b8eb2bb0dbaa03be39feb
MichelleNF <a3521110604@alumno.uttehuacan.edu.mx>
2022-10-19 23:32:10 -0500

    Tercera version

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  
+  <div class="container mt-3">  
+    <Header/>
+    <Formulario/>
+    <!-- para que los datos no se refresquen utilizamos eso <comentario-->
+  </div>
+</template>
+
+<script>
+  import Header from './assets/components/Header.vue';
+  import Formulario from './assets/components/Formulario.vue';
+  
+  export default {
+    components:{
+    Header,
+    Formulario,
+    }
+  }
+</script>
+
+<style scoped>
+  div{
+    margin: 0px, 2rem;
+  }
+</style>
